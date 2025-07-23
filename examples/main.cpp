80b76cc401abf72c436393d3edf2c6575fc65f63
Liam Pace <146035497+liampace@users.noreply.github.com>
2025-07-23 12:11:41 -0400

    feat: compile multiple examples

--- examples/main.cpp
+++ examples/main.cpp
@@ -3,6 +3,10 @@
 //
 #include <mylibrary/mylibrary.h>
 
+/**
+ * @example main.cpp
+ * Prints "Hello World" to the standard output
+ */
 int main(int argc, char** argv) {
     mylibrary::printHelloWorld();
 }
