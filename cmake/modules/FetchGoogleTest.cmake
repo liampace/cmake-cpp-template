db784da05899861bbec7949aa3969dcbca1380de
Liam Pace <146035497+liampace@users.noreply.github.com>
2025-07-30 20:57:19 -0400

    chore: upgrade googletest version

--- cmake/modules/FetchGoogleTest.cmake
+++ cmake/modules/FetchGoogleTest.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 FetchContent_Declare(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG        v1.12.0)
+  GIT_TAG        v1.17.0)
 set(INSTALL_GTEST OFF)
 FetchContent_MakeAvailable(googletest)
 include(GoogleTest)
